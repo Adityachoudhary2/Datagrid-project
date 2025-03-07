ac2c1e28c92fed871fb86d0e10cd1fcd7900fdfb
Aditya <choudharyaditya9381@gmail.com>
2025-03-07 17:37:23 +0530

    done avialable status ready for download and scheduled status is disabled for check

--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -1,12 +1,21 @@
+import React from "react";
+import { DataRow } from "../Data/sampleData"
 import StatusIndicator from "./StatusIndicator"
 
+interface TableRowProps {
+    row: DataRow;
+    selectedRows: string[];
+    handleRowSelection: (name: string) => void; 
 
-const TableRow =({row})=> {
+}
+const TableRow: React.FC<TableRowProps> =({row, selectedRows ,handleRowSelection})=> {
     return (
         <tr>
             <td>
                 <input type="checkbox"
-                  
+                  checked={selectedRows.includes(row.name)}
+                  onChange={() => handleRowSelection(row.name)}
+                  disabled={row.status === "scheduled"}
                 />
             </td>
             <td>{row.name}</td>
